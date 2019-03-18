a97cb86e379955a388c3c3e0d0e8186b4c98f1d0
Emilio <emiliotorrez1990@gmail.com>
2019-03-18 12:22:12 -0600

    structure added

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+
+let dev_db_url = 'mongodb://cafe-user:Cafe2100@ds251002.mlab.com:51002/productsapp';
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+console.log(mongoDB);
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/products', product);
 
 let port = 1234;
 
